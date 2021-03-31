6d179beaa19a95bde0f4a9215e67a4b5a28097bd
Vince P <ianparulan@gmail.com>
2021-03-31 17:35:49 -0400

    chore: maybe this

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,20 @@ import { Calculator } from "../components/calculator"
 
 import ogImg from '../images/og-image.png'
 
-const IndexPage = () => (
-  <Layout>
-    <GatsbySeo
-      title="3D Printing Price Calculator | PriceMy3DPrint"
-      description="Calculate how much you should price your 3D prints right on your browser! Just drop your .STL files and see them instantly."
-      openGraph={{
-        images: [
-          { url: ogImg }
-        ]
-      }}
-    />
-    <Calculator />
-  </Layout>
-)
+const IndexPage = () => {
+  console.log(ogImg)
+  return (
+    <Layout>
+      <GatsbySeo
+        title="3D Printing Price Calculator | PriceMy3DPrint"
+        description="Calculate how much you should price your 3D prints right on your browser! Just drop your .STL files and see them instantly."
+        openGraph={{
+          images: [{ url: `https://pricemy3Dprint.com/${ogImg}` }],
+        }}
+      />
+      <Calculator />
+    </Layout>
+  )
+}
 
 export default IndexPage
